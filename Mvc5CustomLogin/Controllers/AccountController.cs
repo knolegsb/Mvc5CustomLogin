07d2d2d43e1e76f1a91d435896675db9c4d20ad3
Sean <seon2you@hotmail.com>
2016-10-22 21:31:57 -0700

    Add project files.

---
+++ Mvc5CustomLogin/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using Mvc5CustomLogin.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc5CustomLogin.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            //AccountViewModel accview = new AccountViewModel();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(AccountViewModel accview)
+        {
+            
+            if (ModelState.IsValid && accview.Account.Username.Equals("abc") && accview.Account.Password.Equals("123"))
+            {
+                Session["username"] = accview.Account.Username;
+                return View("Welcome");
+            }
+            else
+            {
+                ViewBag.Error = "Account is Invalid!";
+                return View("Index");
+            }            
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Remove("username");
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Mvc5CustomLogin/Models/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Mvc5CustomLogin.Models
+{
+    public class Account
+    {
+        [Display(Name="User Name")]
+        [Required]
+        public string Username { get; set; }
+
+        [Display(Name="Password")]
+        [Required]
+        public string Password { get; set; }
+    }
+}
